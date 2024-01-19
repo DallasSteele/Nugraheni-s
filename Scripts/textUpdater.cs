acedee2fd5b331bcbae2321897365ad57e26b870
Sheerwood Jack Grave <129974988+DallasSteele@users.noreply.github.com>
2024-01-19 16:56:40 +0700

    Add files via upload

---
+++ Scripts/textUpdater.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textUpdater : MonoBehaviour
+{
+   //public Text questionText; // reference UI
+   //public Button answerButton; // reference answer button
+    public string answerText; // text display
+
+    void Start()
+    {}
+
+}
